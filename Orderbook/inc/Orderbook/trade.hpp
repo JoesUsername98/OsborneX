e93b07ae626cbc8175e27da9733c4adf81777e57
JoesUsername98 <joe.osborne1337@gmail.com>
2026-06-05 23:06:57 +0100

    move trade to a new file

---
+++ Orderbook/inc/Orderbook/trade.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "trade_info.hpp"
+
+namespace osbornex {
+
+class Trade
+{
+public:
+    Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+        : bidTrade_ { bidTrade }
+        , askTrade_ { askTrade }
+    { }
+
+    const TradeInfo& GetBidTrade() const { return bidTrade_; }
+    const TradeInfo& GetAskTrade() const { return askTrade_; }
+
+private:
+    TradeInfo bidTrade_;
+    TradeInfo askTrade_;
+};
+
+using Trades = std::vector<Trade>;
+
+} // namespace osbornex
