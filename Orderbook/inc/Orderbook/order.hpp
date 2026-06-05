785215804d72e911fa29dc53bcbc7be7fdc89a67
JoesUsername98 <joe.osborne1337@gmail.com>
2026-06-05 23:23:07 +0100

    move order and order modify to a different file

---
+++ Orderbook/inc/Orderbook/order.hpp
@@ -0,0 +1,61 @@
+# pragma once
+
+#include <expected>
+#include <string>
+#include <format>
+#include <memory>
+#include <list>
+
+#include "domain_types.hpp"
+#include "order_type.hpp"
+#include "side.hpp"
+
+namespace osbornex {
+    
+    class Order
+    {
+    public:
+        Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderType_{ orderType }
+        , orderId_{ orderId }
+        , side_ { side }
+        , price_ { price }
+        , initialQuantity_ { quantity }
+        , remainingQuantity_ { quantity }
+        { }
+    
+        OrderType GetOrderType() const { return orderType_; }
+        OrderId GetOrderId() const { return orderId_; }
+        Side GetSide() const { return side_;  }
+        Price GetPrice() const { return price_; }
+        Quantity GetInitialQuantity() const{ return initialQuantity_; }
+        Quantity GetRemainingQuantity() const { return remainingQuantity_; }
+        Quantity GetFilledQuantity() const { return initialQuantity_ - remainingQuantity_; }
+        bool IsFilled() const { return initialQuantity_ == remainingQuantity_;  }
+    
+        /*[[nodiscard]] std::expected<void, std::string>*/
+        void Fill(Quantity quantity)
+        {
+            if (quantity > GetRemainingQuantity())
+                return; /*std::unexpected(
+                    std::format("OrderID [{}] cannot be filled as the fill quantity [{}] is greater than the remaining quantity [{}]",
+                    GetOrderId(), quantity, GetRemainingQuantity()) 
+                );*/
+    
+            remainingQuantity_ -= quantity;
+        }
+    
+    
+    private:
+        OrderType orderType_;
+        OrderId orderId_;
+        Side side_;
+        Price price_;
+        Quantity initialQuantity_;
+        Quantity remainingQuantity_;
+    };
+
+using OrderPointer = std::shared_ptr<Order>;
+using OrderPointers = std::list<OrderPointer>;
+
+} // namespace osbornex
